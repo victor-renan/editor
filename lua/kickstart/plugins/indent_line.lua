b88a1b1ee421c9de718f8c25c4c4ccbc71a26479
Renan Alves <alvesrenanpro@gmail.com>
2026-03-03 12:29:02 -0300

    layout novo

---
+++ lua/kickstart/plugins/indent_line.lua
@@ -0,0 +1,11 @@
+local data = {
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+  },
+}
+
+return {}
